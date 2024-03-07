ffaf390ac9da1c1d0c0519ab1abe14b8c0a08757
motas <kwhui.gary@gmail.com>
2024-03-07 23:50:21 +0800

    Add display input functions

--- scripts.js
+++ scripts.js
@@ -24,8 +24,41 @@ function divide(num1, num2){
 
 let num1=0, num2=0;
 let operation = "";
+const display = document.querySelector("#display");
 
 function calculate(operation, num1, num2){
     return operations[operation](num1, num2)
 }
 
+function input(num){
+    if (display.textContent === "0"){
+        display.textContent = String(num);
+    } else if (display.textContent.length<9){
+        display.textContent+= num;
+    } 
+}
+
+function clearInput(){
+    display.textContent="0";
+}
+
+function deleteInput(){
+    if (display.textContent.length == 1){
+        display.textContent = "0"
+    } else if(display.textContent.length > 1){
+        let newDisplayContent = display.textContent.slice(0, display.textContent.length-1)
+        display.textContent = newDisplayContent;
+    }
+}
+
+const numButtons = document.querySelectorAll(".number");
+numButtons.forEach(button => 
+    button.addEventListener('click', () => input(button.value))
+    )
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener('click', ()=>clearInput())
+
+
+const delButton = document.querySelector("#delete");
+delButton.addEventListener('click', ()=>deleteInput())
